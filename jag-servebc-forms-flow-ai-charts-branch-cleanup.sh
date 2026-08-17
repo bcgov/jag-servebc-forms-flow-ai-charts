9c37781e6a22dfb7346b271454c90304a26bb553
jaise-aot <jaise.thomas@aot-technologies.com>
2026-08-17 13:17:09 -0700

    forked repo branch cleanup

---
+++ jag-servebc-forms-flow-ai-charts-branch-cleanup.sh
@@ -0,0 +1,26 @@
+git push https://github.com/bcgov/jag-servebc-forms-flow-ai-charts.git --delete \
+  "backup/8.1.0" \
+  "backup/8.2.0" \
+  "backup/8.3.0" \
+  "env-vars" \
+  "feature/8.4.0" \
+  "feature/FWF-4328-nginx-update" \
+  "feature/FWF-4418" \
+  "feature/all-in-one" \
+  "feature/web_display_name" \
+  "fix/remove_all_in_one" \
+  "fwf-3480" \
+  "gh" \
+  "kubernetes" \
+  "master" \
+  "node_config_fix" \
+  "pr_11" \
+  "pr_12" \
+  "sk-k8s" \
+  "sk-k8s-eks" \
+  "sk-k8s-silver" \
+  "staging/QA" \
+  "staging/QA-backup" \
+  "storage-values" \
+  "tag_fix" \
+  "web-v2"
